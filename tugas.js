d47432140940bc4f6d3ecc5e9ead3574c92be278
gilangppe <gilang.putra_ti22@nusaputra.ac.id@gmail.com>
2023-10-05 15:02:09 +0700

    first commit

---
+++ tugas.js
@@ -0,0 +1,10 @@
+let fisrtName = 'Gilang'
+let lastName = 'Putra Pamungkas'
+let age = 19
+let isMaried = 'false'
+
+
+console.log(fisrtName)
+console.log(lastName)
+console.log(age)
+console.log(isMaried)
